1dbee479fa4e509518fcdade1946775d27ebb8ad
MarkoPolo88 <talakhadzemariam@gmail.com>
2023-01-16 19:50:05 +0400

    menu controller

---
+++ admin/helpers/main.js
@@ -0,0 +1,41 @@
+function error_message(message) {
+  return {
+    success: false,
+    message: message,
+  };
+}
+
+function success_message(message) {
+  return {
+    success: true,
+    message: message,
+  };
+}
+function return_data(status, data) {
+  return {
+    success: status,
+    data: data,
+  };
+}
+function return_response(res, status, data) {
+  return res.status(200).send({
+    success: status,
+    data: data,
+  });
+}
+
+function success_response(res, data) {
+  return res.status(200).send(data);
+}
+
+function error_response(res, data) {
+  return res.status(400).send(data);
+}
+
+module.exports = {
+  error_message,
+  success_message,
+  success_response,
+  error_response,
+  return_data,
+};
