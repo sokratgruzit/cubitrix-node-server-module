05c0b1f6e93c10f4d3eba75c45c378f9f025b324
MarkoPolo88 <talakhadzemariam@gmail.com>
2023-01-26 14:50:21 +0400

    removed admin from project

--- admin/routes/data_routes.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express');
-const router = Router();
-
-router.get('/test' ,  (req, res) => {
-   res.send("get some data");
-});
-
-module.exports = router; 
--- app.js
+++ app.js
@@ -6,55 +6,50 @@ const config = require("config");
 const cors = require("cors");
 const cors_options = require("./config/cors_options");
 const credentials = require("./middleware/credentials");
-
-const { accounts } = require("@cubitrix/cubitrix-node-accounts-module");
-const { transactions } = require("@cubitrix/cubitrix-node-globals-module");
-
-const auth = require("./admin/routes/auth_routes");
-const content = require("./admin/routes/content_routes");
-const data = require("./admin/routes/data_routes");
-
-const root = require("path").join(__dirname, "front", "build");
-const app = express();
-
+process.env["NODE_CONFIG_DIR"] = __dirname + "/config";
+const {
+  admin_auth,
+  admin_content,
+  admin_data,
+} = require("@cubitrix/cubitrix-node-admin-module");
 require("dotenv").config();
-process.env["NODE_CONFIG_DIR"] = __dirname + "/admin/config";
-
+const { accounts } = require("@cubitrix/cubitrix-node-accounts-module");
+const app = express();
 app.use(express.json({ extended: true }));
 app.use(credentials);
 app.use(cors(cors_options));
 app.use(
-   bodyParser.urlencoded({
-      extended: true,
-   })
+  bodyParser.urlencoded({
+    extended: true,
+  })
 );
+app.use("/accounts", accounts);
+app.use("/api/auth", admin_auth);
+app.use("/api/content", admin_content);
+app.use("/api/data", admin_data);
+
+app.get("/api/test", (req, res) => {
+  res.send("test");
+});
+//static path
+const root = require("path").join(__dirname, "front", "build");
 app.use(express.static(root));
 
-// Global routes of platform
-app.use("/accounts", accounts);
-
-// Admin routes
-app.use("/api/auth", auth);
-app.use("/api/content", content);
-app.use("/api/data", data);
-
-
-// Db connection and server setup
 async function start() {
-   const PORT = process.env.PORT || 5000;
-   try {
-      mongoose.set("strictQuery", false);
-      await mongoose.connect(process.env.MONGO_URL, {
-         useNewUrlParser: true,
-         useUnifiedTopology: true,
-      });
-      app.listen(PORT, () =>
-         console.log(`App has been started on port ${PORT}...`)
-      );
-   } catch (e) {
-      console.log(`Server Error ${e.message}`);
-      process.exit(1);
-   }
+  const PORT = process.env.PORT || 5000;
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT, () =>
+      console.log(`App has been started on port ${PORT}...`)
+    );
+  } catch (e) {
+    console.log(`Server Error ${e.message}`);
+    process.exit(1);
+  }
 }
 
 start();
