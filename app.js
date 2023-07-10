7ed7f36a358feaba23967ade2158485f03d29726
jinx <talakhadzemariam@gmail.com>
2023-07-10 19:17:49 +0400

    koko

--- app.js
+++ app.js
@@ -18,9 +18,17 @@ const {
 
 require("dotenv").config();
 
-const { accounts, functions } = require("@cubitrix/cubitrix-node-accounts-module");
+const cron = require("node-cron");
+
+const {
+  accounts,
+  functions,
+} = require("@cubitrix/cubitrix-node-accounts-module");
 const { transactions } = require("@cubitrix/cubitrix-node-transactions-module");
-const { referral } = require("@cubitrix/cubitrix-refferal-node-module");
+const {
+  referral,
+  referral_controller,
+} = require("@cubitrix/cubitrix-refferal-node-module");
 const { loan_routes } = require("@cubitrix/cubitrix-node-loan-module");
 
 const octokit = new Octokit({
@@ -38,14 +46,14 @@ app.use(
         req.rawBody = buf.toString();
       }
     },
-  }),
+  })
 );
 app.use(credentials);
 app.use(cors(cors_options));
 app.use(
   bodyParser.urlencoded({
     extended: true,
-  }),
+  })
 );
 const rootDir = process.cwd(); // Get the current working directory
 
@@ -128,6 +136,57 @@ app.post("/api/test", async (req, res) => {
     });
 });
 
+cron.schedule("0 0 * * *", async () => {
+  let daysBetween = getdaysBetween();
+
+  let binary_bv_dayes = "monthly";
+  let uni_days = "daily";
+
+  const currentDate = new Date();
+  const currentDay = currentDate.getDate();
+  const currentDayOfWeek = currentDate.getDay();
+
+  if (uni_days == "daily") {
+    await referral_controller.uni_comission_count(1);
+  } else if ((uni_days = "montly")) {
+    if (currentDay === 1) {
+      await referral_controller.uni_comission_count(daysBetween);
+    }
+  } else if ((uni_days = "weekly")) {
+    if (currentDayOfWeek === 1) {
+      await referral_controller.uni_comission_count(7);
+    }
+  }
+  if (binary_bv_dayes == "daily") {
+    await referral_controller.binary_comission_count(1);
+  } else if ((binary_bv_dayes = "montly")) {
+    if (currentDay === 1) {
+      await referral_controller.binary_comission_count(daysBetween);
+    }
+  } else if ((uni_days = "weekly")) {
+    if (currentDayOfWeek === 1) {
+      await referral_controller.binary_comission_count(7);
+    }
+  }
+});
+
+const getdaysBetween = () => {
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth() + 1;
+  const currentYear = currentDate.getFullYear();
+
+  const previousMonth = currentMonth === 1 ? 12 : currentMonth - 1;
+  const previousYear = currentMonth === 1 ? currentYear - 1 : currentYear;
+
+  const firstDayOfPreviousMonth = new Date(previousYear, previousMonth - 1, 1);
+  const firstDayOfCurrentMonth = new Date(currentYear, currentMonth - 1, 1);
+
+  const daysBetween = Math.round(
+    (firstDayOfCurrentMonth - firstDayOfPreviousMonth) / (1000 * 60 * 60 * 24)
+  );
+  return daysBetween;
+};
+
 setInterval(() => {
   functions.update_current_rates();
 }, 61000);
@@ -145,7 +204,10 @@ async function start() {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
+
+    app.listen(PORT, () =>
+      console.log(`App has been started on port ${PORT}...`)
+    );
   } catch (e) {
     console.log(`Server Error ${e.message}`);
     process.exit(1);
