8f8a1a802a14f5386e32a8254c7406aab3822f8b
David Tavadze <tavadzed@gmail.com>
2023-09-05 00:32:07 +0400

    last

--- config/index.js
+++ config/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 if (!process.env?.JWT_SECRET) throw new Error("you need to provide JWT_SECRET in .env");
 
+// Jwt secret 
 const config = {
   jwtSecret: process.env.JWT_SECRET || "",
 };
